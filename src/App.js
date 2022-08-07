bec756e956258dde70f2fa316c7e4e62d06ac08d
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-08-07 12:44:14 +0100

    begining of formik react by simple  exemple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Formik , Field} from "formik";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  onSubmit = (values) => {
+    console.log(values);
+  }
+  // handleSubmit bach matcharge lpage ajaxifie
+  form = (props) => {
+    return <form onSubmit={props.handleSubmit}> 
+      <label>Name</label><br />
+      <Field name="name" /><br />
+      <label>Email</label><br />
+      <Field name="email" /><br />
+      <button type="submit">Send</button>
+    </form>
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Formik 
+          initialValues={{name: "Raissi Anass",email:""}} 
+          onSubmit={this.onSubmit}  //katl3ab 3la li fo9mminha
+          render={this.form}
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
+
