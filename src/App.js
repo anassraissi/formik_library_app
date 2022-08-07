ff74ba879b0fbffd455a934d1314586a3c3094e9
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-08-07 14:25:34 +0100

    use yup backage with schema form validation form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {Formik , Field} from "formik";
+import {Formik , Field, yupToFormErrors, ErrorMessage} from "formik";
+import * as yup from 'yup';
+//baked-in support for schema-based form-level validation through Yup. 
 
 class App extends Component {
   
@@ -11,32 +13,50 @@ class App extends Component {
     return <form onSubmit={props.handleSubmit}> 
       <label>Name</label><br />
       <Field name="name" /><br />
+      <ErrorMessage name='name'/> <br/>
       <label>Email</label><br />
       <Field name="email"  type='email'/><br />
+      <ErrorMessage name='email'/> <br/>
       <label>Type</label><br />
       <Field name="type"  component='select'> 
+      <option value=''></option>
       <option value='1'>one</option>
       <option value='2'>two</option>
       <option value='3'>tree</option>
-      </Field> 
+      </Field> <br></br> 
+      <ErrorMessage name='type'/> <br/>
+
       <br/>
       <label>Active</label><br />
       <Field name="active"  type='checkbox'/> <br></br> 
+
       <label>Sex</label><br />
       <Field name="sex" type='radio' value="F" /> Femme <br></br> 
       <Field name="sex" type='radio' value="H" /> Homme <br></br> 
+      <ErrorMessage name='sex'/> <br/>
+
 
       <button type="submit">Send</button>
     </form>
+  }
+  Schema=()=>{
+        const schema=yup.object().shape({
+              email: yup.string().required(), //matkonche khawia
+              type: yup.string().required(),  //matkonche khawia
+              sex: yup.string().required(),  //matkonche khawia
+              name: yup.string().required()  //matkonche khawia
+        });
+        return schema;  // return response
   }
 
   render() {
     return (
       <div className="App">
         <Formik 
-          initialValues={{name: "Raissi Anass",email:"",type:'1',active:false,sex:'H'}} // by default  is false 
+          initialValues={{name: "",email:"",type:'',active:false,sex:''}} // by default  is false 
           onSubmit={this.onSubmit}  //katl3ab 3la li fo9mminha
           render={this.form}
+          validationSchema={this.Schema()}
           />
       </div>
     );
