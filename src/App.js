791e5411d003a001d78a9fd42d3a495d6094c30f
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-08-07 13:15:21 +0100

    add some inputs  on form

--- src/App.js
+++ src/App.js
@@ -12,7 +12,20 @@ class App extends Component {
       <label>Name</label><br />
       <Field name="name" /><br />
       <label>Email</label><br />
-      <Field name="email" /><br />
+      <Field name="email"  type='email'/><br />
+      <label>Type</label><br />
+      <Field name="type"  component='select'> 
+      <option value='1'>one</option>
+      <option value='2'>two</option>
+      <option value='3'>tree</option>
+      </Field> 
+      <br/>
+      <label>Active</label><br />
+      <Field name="active"  type='checkbox'/> <br></br> 
+      <label>Sex</label><br />
+      <Field name="sex" type='radio' value="F" /> Femme <br></br> 
+      <Field name="sex" type='radio' value="H" /> Homme <br></br> 
+
       <button type="submit">Send</button>
     </form>
   }
@@ -21,7 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <Formik 
-          initialValues={{name: "Raissi Anass",email:""}} 
+          initialValues={{name: "Raissi Anass",email:"",type:'1',active:false,sex:'H'}} // by default  is false 
           onSubmit={this.onSubmit}  //katl3ab 3la li fo9mminha
           render={this.form}
           />
