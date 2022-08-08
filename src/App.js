dc181559816de9d20bdbfc1df14cfd9c70218dcb
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-08-08 14:26:09 +0100

    field array on form by using formik library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Formik , Field, yupToFormErrors, ErrorMessage} from "formik";
+import {Formik , Field, yupToFormErrors, ErrorMessage,FieldArray} from "formik";
 import * as yup from 'yup';
 //baked-in support for schema-based form-level validation through Yup. 
 
@@ -42,7 +42,25 @@ class App extends Component {
  <label>Facebook</label><br />
       <Field name="social.facebook" /><br />
       <ErrorMessage name='social.facebook'/> <br/>
+      
+      <FieldArray
+              name='friends'
+              render={arrayHelper=>(
+                    <div>
 
+                    {props.values.friends.map((item,index)=>(
+                      <div key={index}>
+                      <Field name={`friends.${index}`} />
+                      <button type='button' onClick={()=>arrayHelper.remove(index)}> - </button>
+                      <ErrorMessage name={`friends.${index}`}/> <br/>
+                      </div>
+
+                      ))}
+                        <button type='button'  onClick={()=>arrayHelper.push('')}>Add</button>
+                      </div>
+                    
+              )} 
+      />
       <button type="submit">Send</button>
     </form>
   }
@@ -54,8 +72,11 @@ class App extends Component {
               name: yup.string().required(),  //matkonche khawia
               social:yup.object().shape({
                 facebook:yup.string().required('Facebook is a required field'), //personlise the output of error message
-                twitter:yup.string().required('Twitter is a required field')
+                twitter:yup.string().required('Twitter is a required field'),
               }),
+              friends:yup.array().of(
+                yup.string().required('friend field Required ')
+            )
         });
         return schema;  // return response
   }
@@ -71,7 +92,8 @@ class App extends Component {
           sex:'',
           social:{
               facebook:'',twitter:''
-          }
+          },
+          friends:['Raissi anass','Imam']
               
         }} // by default  is false 
           onSubmit={this.onSubmit}  //katl3ab 3la li fo9mminha
